914fafa34f40d9490ef3f470e599ee82d1eea3dd
Gustavo-Henrique-Lima <gustavohgustavo@hotmail.com>
2026-02-07 16:24:28 -0300

    chore: Pagination on raw material.

--- stock/src/main/java/com/gustavonascimento/stock/usecases/rawmaterial/ListRawMaterialsUseCase.java
+++ stock/src/main/java/com/gustavonascimento/stock/usecases/rawmaterial/ListRawMaterialsUseCase.java
@@ -2,6 +2,10 @@ package com.gustavonascimento.stock.usecases.rawmaterial;
 
 import com.gustavonascimento.stock.records.rawmaterial.GetRawMaterial;
 import com.gustavonascimento.stock.repositories.RawMaterialRepository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,16 +26,20 @@ public class ListRawMaterialsUseCase {
     }
 
     @Transactional
-    public List<GetRawMaterial> execute() {
-        LOG.info("Buscando lista de todas as máterias-primas cadastradas no DB");
-        return repository.findAll()
-                .stream()
+    public Page<GetRawMaterial> execute(Pageable pageable) {
+        LOG.info(
+                "Buscando matérias-primas página={} tamanho={} ordenação={}",
+                pageable.getPageNumber(),
+                pageable.getPageSize(),
+                pageable.getSort()
+        );
+
+        return repository.findAll(pageable)
                 .map(entity -> new GetRawMaterial(
                         entity.getId(),
                         entity.getCode(),
                         entity.getName(),
                         entity.getStockQuantity()
-                ))
-                .collect(Collectors.toList());
+                ));
     }
 }
